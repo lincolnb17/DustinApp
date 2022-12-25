7432e971dd52198bd900a8e991f9be0ad62cb529
lincolnb17 <bhattachanl17@gmail.com>
2022-12-25 18:34:13 +0545

    test

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
   return (
 <nav className="navbar sticky-top navbar-expand-lg bg-light">
   <div className="container">
-  <Link className="nav-link active" to="/home"><a className="navbar-brand">Dust-In Test Github Test 1 Jenkins 2</a></Link>
+  <Link className="nav-link active" to="/home"><a className="navbar-brand">Dust-In </a></Link>
     
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
